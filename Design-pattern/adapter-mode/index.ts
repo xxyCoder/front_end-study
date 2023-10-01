7c51885d0ad1f27fa663a16e3d02da3082e982ce
xxy <1696944145@qq.com>
2023-10-01 12:20:14 +0800

    learn design pattern

---
+++ Design-pattern/adapter-mode/index.ts
@@ -0,0 +1,43 @@
+interface MediaPlayer {
+    play(audioType: string, filename: string): void;
+}
+
+interface AdvancedMediaPlayer {
+    playVlc(filename: string): void;
+    playMp4(filename: string): void;
+}
+
+class VlcPlayer implements AdvancedMediaPlayer {
+    playVlc(filename: string): void {
+        console.log(`Playing vlc file ${filename}`)
+    }
+    playMp4(filename: string): void {
+        // 什么也不做
+    }
+}
+class Mp4Player implements AdvancedMediaPlayer {
+    playVlc(filename: string): void {
+        // 什么也不做
+    }
+    playMp4(filename: string): void {
+        console.log(`Playing mp4 file ${filename}`)
+    }
+}
+
+class MediaAdapter implements MediaPlayer {
+    advancedMusicPlayer: AdvancedMediaPlayer;
+    constructor(audioType: string) {
+        if (audioType === "vlc") {
+            this.advancedMusicPlayer = new VlcPlayer();
+        } else {
+            this.advancedMusicPlayer = new Mp4Player();
+        }
+    }
+    play(audioType: string, filename: string): void {
+        if (audioType === "vlc") {
+            this.advancedMusicPlayer.playVlc(filename);
+        } else {
+            this.advancedMusicPlayer.playMp4(filename);
+        }
+    }
+}
