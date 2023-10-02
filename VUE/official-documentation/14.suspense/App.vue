335cdaea4a54b674c9e52cd34acbcece651d88a4
xxy <1696944145@qq.com>
2023-10-02 23:01:03 +0800

    learn vue

--- VUE/official-documentation/14.suspense/App.vue
+++ VUE/official-documentation/14.suspense/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Comp from './Comp.vue';
+import Comp1 from './Comp1.vue';
 </script>
 
 <template>
     <Suspense>
         <Comp />
+        <template #fallback>Loading2....</template>
+    </Suspense>
+    <Suspense>
+        <Comp1 />
         <template #fallback>Loading1....</template>
     </Suspense>
 </template>
---
+++ VUE/official-documentation/14.suspense/Comp1.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const msg = ref('Hello World!')
+
+await new Promise(resolve => {
+    setTimeout(() => {
+        msg.value = "xxyCoder";
+        resolve();
+    }, 3000);
+})
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <input v-model="msg">
+</template>
