93d1a98df9e35948724ffefef99a36c574d8fc3f
xxy <1696944145@qq.com>
2023-10-02 22:37:50 +0800

    review nodejs

--- Node/technology-stack_book/01.system-modules/commonjs/index.js
+++ Node/technology-stack_book/01.system-modules/commonjs/index.js
@@ -1,7 +1,12 @@
-console.log('b.js')
+console.log('b.js', this)
 exports.test = 2;
 const a = require('./index.module')
 exports.bbb = 'b'
+
+setTimeout(() => {
+    this.bbb = "aaa";
+    this.y = 2;
+}, 5000)
 console.log('undeclaredVariable: ', undeclaredVariable);
 
 console.log('b模块加载完毕: a.test值:', a.test);
--- Node/technology-stack_book/01.system-modules/es-module/calc.mjs
+++ Node/technology-stack_book/01.system-modules/es-module/calc.mjs
@@ -1,3 +1,11 @@
 export function add(a, b) {
     return a + b;
 }
+
+console.log("calc.mjs", this)
+
+export let a = 2;
+
+setTimeout(() => {
+    a = 3;
+}, 5000)
