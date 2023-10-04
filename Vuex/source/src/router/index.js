d06838cc9578759ed29e6b85988fb673d06c2515
xxy <1696944145@qq.com>
2023-10-04 14:48:39 +0800

    learn vuex

---
+++ Vuex/source/src/router/index.js
@@ -0,0 +1,45 @@
+import { inject, reactive } from 'vue'
+const STORE_KEY = '__store__';
+
+function useStore() {
+    return inject(STORE_KEY);
+}
+
+class Store {
+    constructor(options) {
+        this._state = reactive({
+            ...typeof options.state === 'function' ? options.state() : options.state
+        });
+        options.getters && Object.getOwnPropertyNames(options.getters).forEach(key => {
+            this._getter[key] = computed(() => options.getters[key](this.state));
+        })
+        this._mutations = options.mutations;
+        this._actions = options.actions;
+    }
+    install(app) {
+        app.provide(STORE_KEY, this);
+    }
+    get state() {
+        return this._state.data;
+    }
+    get getters() {
+        return this._getter;
+    }
+    commit(type, payload) {
+        const entry = this.mutations[type];
+        entry && entry(this.state, payload);
+    }
+    dispatch(type, payload) {
+        const entry = this.actions[type];
+        entry && entry({ commit: this.commit }, payload);
+    }
+}
+
+function createStore(options) {
+    return new Store(options);
+}
+
+export {
+    createStore,
+    useStore
+}
