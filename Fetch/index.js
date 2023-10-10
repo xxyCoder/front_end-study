ef68ae8958b81ac888e29ba1deddc9ff04082627
xxy <1696944145@qq.com>
2023-10-10 13:46:30 +0800

    learn fetch

--- Fetch/index.js
+++ Fetch/index.js
@@ -1,42 +1,39 @@
-// fetch("http://localhost:3000")
-//     .then(async response => {
-//         // response是流对象
-//         console.log("同步属性:", response.status, response.statusText)
-//         response.headers.forEach((v, k) => console.log(k, ":", v))
-//         if (response.status >= 200 && response.status < 300) {  // 判断是否请求成功，或者使用response.ok属性判断
-//             const reader = response.body.getReader();
-//             while (true) {
-//                 const { done, value } = await reader.read();
-//                 if (done) {
-//                     break;
-//                 }
-//                 console.log(`Receiver ${value.length} bytes`)
-//             }
-//             // return response.text(); // 异步操作，取出所有内容，并转换为JSON对象
-//         } else {
-//             throw new Error(response.statusText)
-//         }
-//     })
-//     .then(json => {
-//         console.log(json);
-//     })
-//     .catch(err => console.log(err))
+fetch("http://localhost:3000")
+    .then(async response => {
+        // response是流对象
+        console.log("同步属性:", response.status, response.statusText)
+        response.headers.forEach((v, k) => console.log(k, ":", v))
+        if (response.status >= 200 && response.status < 300) {  // 判断是否请求成功，或者使用response.ok属性判断
+            const reader = response.body.getReader();
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    break;
+                }
+                console.log(`Receiver ${value.length} bytes`)
+            }
+            // return response.text(); // 异步操作，取出所有内容，并转换为JSON对象
+        } else {
+            throw new Error(response.statusText)
+        }
+    })
+    .catch(err => console.log(err))
 
 // 取消请求
 const controller = new AbortController();
 setTimeout(() => controller.abort(), 1000)
 
-// const respponse = await fetch("http://localhost:3000", {
-//     method: "POST",
-//     headers: {
-//         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-//     },
-//     body: "foo=bar&name=xxyCoder",
-//     mode: "cors",
-//     keepalive: true,
-//     signal: controller.signal
-// })
-// console.log(await respponse.json())
+const respponse = await fetch("http://localhost:3000", {
+    method: "POST",
+    headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    body: "foo=bar&name=xxyCoder",
+    mode: "cors",
+    keepalive: true,
+    signal: controller.signal
+})
+console.log(await respponse.json())
 
 fetch("http://localhost:3000", {
     method: "GET",
--- Fetch/readme.md
+++ Fetch/readme.md
@@ -1,10 +1,16 @@
 # Fetch https://www.ruanyifeng.com/blog/2020/12/fetch-tutorial.html
 - 功能与XMLHttpRequest基本相同，但有三个主要的差异
-  - fetch使用Promise，不使用回调函数
+  - fetch使用Promise，不使用回调函数，必须是异步，XHR可选是否异步
   - 采用模块化设计，API分散在多个对象上（Response、Request、Headers）
   - 通过数据流处理数据
 - fetch成功后，得到一个Response对象
 - 只有网络错误或无法连接，fetch才会报错，也就是说即使服务器返回的状态是4xx或5xx，Promise也不会是reject状态
+
+## Response
+- status  状态码
+- statuxText  状态信息
+- redirected  是否重定向了
+- ok  状态码200~299之间为true
 
 ## 读取内容方法
 - response.text()   得到文本字符串
@@ -19,7 +25,7 @@
 
 ## cache
 - default 默认值，先从缓存中找
-- no-store
+- no-store  直接发送请求
 - reload  直接请求服务器，并更新缓存
 - no-cache  类似协商缓存
 - force-cache 缓存优先，只有不存在缓存才会请求远程
@@ -34,3 +40,18 @@
 - same-origin 默认值，同源请求发送cookie
 - include 不论同源还是跨域都发送cookie
 - omit  都不发送
+
+## signal
+- 支持通过AbortController中断fetch请求
+
+# Header对象
+- 所有外发请求和入站响应体部的容器
+- 与Map有相似之处，相同的方法，但是初始化可使用对象初始化
+
+# Request对象
+- 获取资源的请求接口
+- 接收一个URL作为参数，第二个参数是init对象
+
+# Response对象
+- 获取资源响应的接口
+- 初始化不需要URL参数，有可选body参数，init参数
