044f06d468f189baf42e3409fa2c01f5f039c5b8
xxy <1696944145@qq.com>
2023-10-21 17:05:23 +0800

    learn micro-app

---
+++ micro-front-end/micro-app-use/substrate/src/page1.js
@@ -0,0 +1,6 @@
+export default function page1() {
+    return <div>
+        React项目
+        <micro-app name='app1' url='http://localhost:10000/' baseroute='/react'></micro-app>
+    </div>
+}
---
+++ micro-front-end/micro-app-use/substrate/src/page2.js
@@ -0,0 +1,6 @@
+export default function page2() {
+    return <div>
+        Vue项目
+        <micro-app name='app2' url='http://localhost:8080/' baseroute='/vue'></micro-app>
+    </div>
+}
