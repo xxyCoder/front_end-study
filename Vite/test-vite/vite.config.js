1f2dcc1e058168539feb65053c6b5e6244e69699
xxy <1696944145@qq.com>
2023-11-03 20:41:27 +0800

    learn:vite env

--- Vite/test-vite/vite.config.js
+++ Vite/test-vite/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import viteBaseConfig from './vite.base.config';
 import viteDevConfig from './vite.dev.config';
 import viteProConfig from './vite.pro.config';
@@ -8,6 +8,9 @@ const envResolver = {
     "serve": () => Object.assign({}, viteBaseConfig, viteDevConfig)
 }
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+
+    const env = loadEnv(mode, process.cwd(), "");   // 第三个参数为文件名，默认为 .env
+    console.log("env:", env);
     return envResolver[command]();
 })
