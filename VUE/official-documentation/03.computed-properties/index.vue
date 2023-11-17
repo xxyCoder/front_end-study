896252c965602346c2a5e9de70469d723a5ad15e
xxy <1696944145@qq.com>
2023-11-17 17:44:21 +0800

    learn:vue

--- VUE/official-documentation/03.computed-properties/index.vue
+++ VUE/official-documentation/03.computed-properties/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onBeforeUpdate, onUpdated } from 'vue'
 
 const count1 = ref(0);
 const count2 = ref(0);
@@ -14,11 +14,17 @@ const double = computed({
 })
 let dt = double;
 console.log(dt === double);
+onBeforeUpdate(() => {
+    console.log("before update");
+})
+onUpdated(() => {
+    console.log("update");
+})
 </script>
 
 <template>
     <div>
-        {{ count2 }} --- {{ double }}
+        {{ double }}
         <button @click="() => count1++">+1</button>
         <button @click="() => { count2++; console.log(dt === double); }">+2</button>
     </div>
