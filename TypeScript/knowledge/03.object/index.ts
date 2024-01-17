fd29dc4cceeafd762389cb3b672c4d40b6051a56
xxycoderxu_i <xxycoderxu_i@didiglobal.com>
2024-01-17 20:14:20 +0800

    feat: 学习ts的对象类型

--- TypeScript/knowledge/03.object/index.ts
+++ TypeScript/knowledge/03.object/index.ts
@@ -61,3 +61,65 @@ A = B;
 // 空对象
 const obj = {};
 // obj.prop = 123; error
+
+const obj5 = {
+    name: "xxy"
+} as const;
+
+// const point1: {
+//     x: number;
+//     y: number;
+// } = {
+//     x: 1,
+//     y: 1,
+//     z: 1 // 报错
+// };
+
+const myPoint = {
+    x: 1,
+    y: 1,
+    z: 1
+};
+
+const point2: {
+    x: number;
+    y: number;
+} = myPoint; // 正确
+
+const point3: {
+    x: number;
+    y: number;
+} = {
+    x: 1,
+    y: 1,
+    z: 1
+} as { x: number, y: number, z: number };
+
+type Options = {
+    title: string;
+    darkMode?: boolean;
+};
+
+// const obj6: Options = {
+//     title: '我的网页',
+//     darkmode: true, // 报错
+// };
+
+let myOptions = {
+    title: '我的网页',
+    darkmode: true,
+};
+// 如果没有严格字面量规则，就不会报错，因为darkMode是可选属性，根据结构类型原则，任何对象只要有title属性，都认为符合Options类型
+const obj7: Options = myOptions;
+
+type OptionsPair = {
+    a?: number;
+    b?: number;
+    c?: number;
+};
+
+const opts = { d: 123 };
+
+// const obj8: OptionsPair = opts; // 报错
+
+export { }
